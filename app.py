59c6ddec0fa4f793ff1c38b607de8ae5274f2aab
Mike Rohr <mike@Mikes-MacBook-Pro.local>
2016-03-31 22:38:18 +0200

    test env variable

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
+import os
 from flask import Flask
+
 app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
 
 
 @app.route('/')
 def hello():
+    print(os.environ['APP_SETTINGS'])
     return "Hello World!"
-
 
 @app.route('/<name>')
 def hello_name(name):

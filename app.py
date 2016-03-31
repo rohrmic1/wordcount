077327436c054420d5c9b18677b4976723d40052
Mike Rohr <mike@Mikes-MacBook-Pro.local>
2016-03-31 23:05:40 +0200

    fixed test env var

--- app.py
+++ app.py
@@ -3,11 +3,10 @@ from flask import Flask
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
-
+print(os.environ['APP_SETTINGS'])
 
 @app.route('/')
 def hello():
-    print(os.environ['APP_SETTINGS'])
     return "Hello World!"
 
 @app.route('/<name>')
